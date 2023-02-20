27ffc5331fb2a296a26f2fcd05e04ac78635b86a
CharlieMickey <charles.f.mickey@gmail.com>
2023-02-20 14:28:32 -0600

    mvp ttt functionality

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 "use strict";
 //constants
+const idleMssg = 'press start to play';
 const winMssg = 'has won!';
 const tieMssg = 'Its a Cat\'s game meeeow';
 const activeMssg = 'the game is afoot';
 const winConditions = [[0, 1, 2], [0, 4, 8], [0, 3, 6], [1, 4, 7], [2, 4, 6], [2, 5, 8], [3, 4, 5], [6, 7, 8]];
 //Variables
-let board;
-let turn;
-let winner;
+let board = ['', '', '', '', '', '', '', '', ''];
+let turn = 0;
+let winner = { status: true, winner: '' };
 let tie;
 //cached element references
 const statusMssg = document.querySelector('#status-message');
@@ -16,47 +17,102 @@ for (let i = 0; i < 9; i++) {
     let sqr = document.querySelector(`#sq${i}`);
     boardGrab?.push(sqr);
 }
+console.log(boardGrab);
 const gameBoard = boardGrab;
+const resetBtn = document.querySelector('#reset-button');
+const startBtn = document.querySelector('#start-button');
 //event listeners
+gameBoard.forEach(sqr => { sqr?.addEventListener('click', function () { handleClick(sqr); }); });
+startBtn.addEventListener('click', function () {
+    init();
+    statusMssg.innerText = activeMssg;
+});
+resetBtn.addEventListener('click', function () { reset(); });
 //functions
 function init() {
+    if (turn !== 0)
+        return;
     initBoard();
     turn = -1;
-    winner = 0;
+    winner.status = false;
     tie = false;
     render();
+    console.log('inited');
 }
 function initBoard() {
-    board = [null, null, null, null, null, null, null, null, null];
+    board = ['', '', '', '', '', '', '', '', ''];
 }
 function render() {
     renderBoard();
     renderMessage();
 }
 function renderBoard() {
-    for (let i = 0; i < board.length; i++) {
+    for (let i = 0; i < gameBoard.length; i++) {
+        board[i] = gameBoard[i].innerHTML;
     }
 }
 function renderMessage() {
-    if (statusMssg)
-        statusMssg.textContent = 'test';
+    if (tie) {
+        statusMssg.innerText = tieMssg;
+    }
+    else if (winner.status === true && winner.winner !== '') {
+        {
+            statusMssg.innerText = winner.winner + ' ' + winMssg;
+        }
+    }
 }
-function handleClick() {
-    return false;
+function handleClick(evt) {
+    placePiece(evt);
+    render();
+    checkForWin();
+    checkForTie();
+    render();
 }
-function placePiece() {
+function placePiece(evt) {
+    if (evt.innerHTML !== '' && turn !== 0) {
+        statusMssg.innerHTML = 'try again';
+        return;
+    }
+    if (turn === -1) {
+        evt.innerHTML = 'X';
+        statusMssg.innerText = 'Now O';
+    }
+    else if (turn === 1) {
+        evt.innerHTML = 'O';
+        statusMssg.innerText = 'Now x';
+    }
+    switchTurn();
 }
 function switchTurn() {
+    if (winner.status === false) {
+        turn *= -1;
+    }
 }
 function checkForWin() {
+    winConditions.forEach(arr => {
+        let temp = [board[arr[0]], board[arr[1]], board[arr[2]]];
+        if (temp.every(el => { return el === 'X'; }) || temp.every(el => { return el === 'O'; })) {
+            winner.status = true;
+            winner.winner = temp[0];
+            turn = 0;
+        }
+    });
 }
 function checkForTie() {
+    if (winner.status === true || board.some(sqr => { return sqr === ''; })) {
+        tie = false;
+    }
+    else if (board.every(sqr => { return sqr !== ''; })) {
+        tie = true;
+        turn = 0;
+    }
 }
 function resetBoard() {
-    board.forEach(square => { square = null; });
+    gameBoard.forEach(square => { square.innerHTML = ''; });
+    renderBoard();
 }
 function reset() {
     resetBoard();
-    init();
+    turn = 0;
+    statusMssg.innerText = idleMssg;
 }
-renderMessage();
--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,54 @@
 //constants
+const idleMssg:string = 'press start to play'
 const winMssg:string = 'has won!';
 const tieMssg:string = 'Its a Cat\'s game meeeow'
 const activeMssg:string = 'the game is afoot'
 const winConditions:number[][]= [[0,1,2],[0,4,8],[0,3,6],[1,4,7],[2,4,6],[2,5,8],[3,4,5],[6,7,8]];
+type gameStatus ={
+  status:boolean,
+  winner:string
+}
 //Variables
-let board: (string|null)[];
-let turn:number;
-let winner:number;
+let board: (string)[]=['','','','','','','','',''];
+let turn:number = 0;
+let winner:gameStatus={status:true,winner:''};
 let tie:boolean;
+
 //cached element references
-const statusMssg: HTMLHeadElement|null = document.querySelector<HTMLHeadingElement>('#status-message')
-let boardGrab:(HTMLDivElement|null)[]=[];
+const statusMssg = document.querySelector<HTMLHeadingElement>('#status-message')!
+let boardGrab:HTMLDivElement[]=[];
 for (let i:number=0; i<9;i++){
-  let sqr = document.querySelector<HTMLDivElement>(`#sq${i}`)
+  let sqr = document.querySelector<HTMLDivElement>(`#sq${i}`)!
   boardGrab?.push(sqr)
 }
-const gameBoard: (HTMLDivElement|null)[]= boardGrab
+console.log(boardGrab)
+const gameBoard: HTMLDivElement[]= boardGrab
+const resetBtn= document.querySelector<HTMLButtonElement>('#reset-button')!
+const startBtn = document.querySelector<HTMLButtonElement>('#start-button')!
 //event listeners
+gameBoard.forEach(sqr=>{sqr?.addEventListener('click', function(){handleClick(sqr)})});
+
+startBtn.addEventListener('click', function(){
+  init()
+  statusMssg.innerText = activeMssg
+})
+
+resetBtn.addEventListener('click', function(){reset()})
+
 
 
 //functions
-function init(){
+function init():void{
+  if(turn !== 0) return
   initBoard();
   turn=-1;
-  winner=0;
+  winner.status = false;
   tie=false;
   render()
+  console.log('inited')
 }
 function initBoard(){
-  board = [null,null,null,null,null,null,null,null,null]
+  board = ['','','','','','','','','']
 }
 
 function render():void{
@@ -37,44 +57,75 @@ renderMessage();
 }
 
 function renderBoard():void{
-  for (let i:number=0;i<board.length;i++){
-
+  for (let i:number=0;i<gameBoard.length;i++){
+    board[i] = gameBoard[i].innerHTML
   }
 }
 
 function renderMessage():void{
-  if(statusMssg) statusMssg.textContent= 'test'
+  if(tie){statusMssg.innerText = tieMssg}
+  else if(winner.status === true && winner.winner !== ''){
+  {statusMssg.innerText = winner.winner + ' ' + winMssg}
+
+  }
 }
 
-function handleClick():boolean{
-  return false
+function handleClick(evt:HTMLDivElement):void{
+  placePiece(evt)
+  render()
+  checkForWin()
+  checkForTie()
+  render()
 }
 
-function placePiece():void{
-
+function placePiece(evt:HTMLDivElement):void{
+    if(evt.innerHTML !== '' && turn !== 0){
+      statusMssg.innerHTML = 'try again'
+      return
+    }
+    if(turn === -1){
+      evt.innerHTML = 'X'
+      statusMssg.innerText = 'Now O'
+    }else if(turn === 1){
+      evt.innerHTML = 'O'
+      statusMssg.innerText = 'Now x'
+    }
+    switchTurn()
 }
 
 function switchTurn():void{
-
+  if (winner.status === false){
+    turn *= -1;
+  }
 }
 
 function checkForWin():void{
-
+  winConditions.forEach(arr =>{
+    let temp:string[]= [board[arr[0]],board[arr[1]],board[arr[2]]];
+    if(temp.every(el =>{return el === 'X'})||temp.every(el =>{return el === 'O'})){
+      winner.status = true;
+      winner.winner = temp[0];
+      turn=0;
+    }
+  })
 }
 
 function checkForTie():void{
-
+  if(winner.status === true || board.some(sqr => {return sqr === ''})) {tie = false}
+  else if (board.every(sqr =>{ return sqr !== ''})) {
+    tie = true
+    turn = 0
+    }
 }
 
 
 function resetBoard(){
-  board.forEach(square=>{square = null})
+  gameBoard.forEach(square=>{square.innerHTML = ''})
+  renderBoard()
 }
 
 function reset():void{
   resetBoard();
-  init();
-  
+  turn = 0;
+  statusMssg.innerText = idleMssg
 }
-
-renderMessage()
